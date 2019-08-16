bab0b1a317efb5c1d33810a33767db7084b74e84
tavisca-rdimri <51902529+tavisca-rdimri@users.noreply.github.com>
2019-08-16 11:15:53 +0530

    Update Program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace WebApi
+namespace WebApplication10
 {
     public class Program
     {
@@ -19,6 +19,7 @@ namespace WebApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()..UseUrls(urls: "http://*:3456");;
+                .UseStartup<Startup>()
+            .UseUrls(urls: "http://*:5678");
     }
 }
